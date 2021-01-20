2015d7cf628c22c03beef67615605112a0a2b25e
Vaibhav <sisodiasinghvaibhav@gmail.com>
2021-01-20 17:53:40 +0530

    renamed. made more responsive. apparently ready for deployment

--- flasknchat/__init__.py
+++ flasknchat/__init__.py
@@ -5,10 +5,10 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
-app.secret_key = '9b907c7aff32d2ce5acebd1ec1bf6f67'
+app.secret_key = 'changeme'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 login = LoginManager(app)
 
-from chatriya import routes
+from flasknchat import routes
